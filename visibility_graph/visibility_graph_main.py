6fee8589b3d1cd6fc76b6d7394d4718e6b54e547
FrancescaRagazzi <francescaragazzi9@gmail.com>
2023-07-31 20:12:48 +0200

    graph

--- visibility_graph/visibility_graph_main.py
+++ visibility_graph/visibility_graph_main.py
@@ -15,48 +15,25 @@ from lib.models.visibility_graph_robot2D_2wheels import *
 from lib.models.obstacle import scale_polygon
 
 
-
 env = Environment(1000, 600)
 
-#first
 obstacles = [
-    Obstacle([(190, 260), (100, 350), (290, 440)]),
-    Obstacle([(450, 160), (540, 90), (840, 340), (740, 540)]),
+    Obstacle([(100, 350),(290, 440),(190, 260) ]),
+    Obstacle([(450, 160),(740, 540), (840, 340), (540, 90)]),
     Obstacle([(700,50), (770,100), (800,20)])
 ]
-
-#second
-# obstacles = [
-#     Obstacle([(250, 250), (400, 250), (400, 400),(250,400)]),
-#     Obstacle([(430, 110), (700, 150), (600, 350)]),
-#     Obstacle([(600,550), (800,550), (700,350)])
-# ]
-
-#third
-# obstacles = [
-#     Obstacle([(100, 100), (100, 700), (300, 700),(300,100)]),
-#     Obstacle([(600,0), (600, 400), (800,400),(800,0)]),
-# ]
-
 
 for obstacle in obstacles:
     env.add_obstacle(obstacle)
 
-#first
-visibility = VisibilityGraph(env, (100, 500), (900, 50)) #(880, 280)
+visibility = VisibilityGraph(env, (100, 500), (900, 50)) 
 
-#second
-# visibility = VisibilityGraph(env, (100, 500), (800, 50))
-
-#third
-# visibility = VisibilityGraph(env, (10, 500), (950, 500))
-
-visibility.find_paths()
+visibility.find_fastest_path()
 
 for point in visibility.fastest_path:
     print(point)
 
-scale_factor = 0.7
+scale_factor = 0.75
 scaled_obstacles = []
 for obstacle in obstacles:
     scaled_obstacles.append(obstacle.scale(scale_factor))
@@ -72,5 +49,37 @@ if __name__ == '__main__':
     sys.exit(app.exec_())
 
 
+
+
+
+
+
+
+
+
+
+
+#second
+# obstacles = [
+#     Obstacle([(250, 250), (400, 250), (400, 400),(250,400)]),
+#     Obstacle([(430, 110), (700, 150), (600, 350)]),
+#     Obstacle([(600,550), (800,550), (700,350)])
+# ]
+
+# for obstacle in obstacles:
+#     env.add_obstacle(obstacle)
+
+# visibility = VisibilityGraph(env, (100, 500), (800, 50))
+
+#third
+# obstacles = [
+#     Obstacle([(100, 100), (100, 600), (300, 600),(300,100)]),
+#     Obstacle([(600,0), (600, 400), (800,400),(800,0)]),
+# ]
+
+# for obstacle in obstacles:
+#     env.add_obstacle(obstacle)
+
+# visibility = VisibilityGraph(env, (10, 400), (950, 500))
 
 
